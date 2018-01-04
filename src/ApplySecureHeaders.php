1e44407f14543a968b1e9d554af5abb4968e7a1c
Aidan Woods <aidantwoods@gmail.com>
2018-01-04 00:52:16 +0000

    Format comments

--- src/ApplySecureHeaders.php
+++ src/ApplySecureHeaders.php
@@ -97,7 +97,7 @@ class ApplySecureHeaders
     }
 
     /**
-     * Set safe mode, if it is required.
+     * Set safe or (inclusive) strict mode, if it is required.
      *
      * @return void
      */
